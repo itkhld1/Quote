6c7ae16e99592957b44f6edaad646b6dc30d4375
Khaled Samim <126877550+itkhld1@users.noreply.github.com>
2025-07-04 15:59:20 +0300

    Initial Commit

---
+++ Quote Watch App/QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteApp.swift
+//  Quote Watch App
+//
+//  Created by itkhld on 4.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Quote_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
